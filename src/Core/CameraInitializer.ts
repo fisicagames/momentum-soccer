63e5cb0abb5d9c3955c2a3b165c82db0278b95ab
fisicagames <rafaelj3d@gmail.com>
2026-06-13 11:20:46 -0300

    feat(camera): remove attach control

--- src/Core/CameraInitializer.ts
+++ src/Core/CameraInitializer.ts
@@ -24,7 +24,7 @@ export class CameraInitializer {
         //camera.setTarget(new Vector3(0, 5.31, 7.84));
         //TODO: [x] Add or remove the attachControl bellow to control the camera with mouse:
         if(canvas && false){
-            camera.attachControl(canvas, true);
+            //camera.attachControl(canvas, true);
         }
         return camera;
     }
